a066cfe6b2dc83e50ddb56079f6e6dbb54557732
Khushisoni1224 <khushisoni1224@gmail.com>
2026-04-06 23:49:51 +0530

    fix: update routing for Aetheron chat interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Chat from './pages/Chat/Chat'; 
+import AppRoutes from "./routes/AppRoutes";
+import "./Styles/global.css";
 
-const AppRoutes = () => {
-  return (
-    /* Add basename so the router knows to ignore the /Aetheron/ part of the URL */
-    <Router basename="/Aetheron">
-      <Routes>
-        <Route path="/" element={<div>Home Page (Default)</div>} /> 
-        
-        {/* This will now correctly match /Aetheron/chat */}
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </Router>
-  );
-};
+function App() {
+  return <AppRoutes />;
+}
 
-export default AppRoutes;
+export default App;
